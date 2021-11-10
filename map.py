d476dc72430eb402b0c8149e28f0f5076eac956b
Coltin Kunz <coltin@visimo.ai>
2021-11-10 17:27:56 -0500

    Map file

---
+++ map.py
@@ -0,0 +1,18 @@
+import os
+import pygame
+
+map_img = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("images", "map.png")).convert_alpha()
+)
+
+
+class Map:
+    def __init__(self):
+        self.img = map_img
+        self.mask = self.create_mask()
+
+    def create_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+    def draw(self, win):
+        win.blit(self.img, (0, 0))
